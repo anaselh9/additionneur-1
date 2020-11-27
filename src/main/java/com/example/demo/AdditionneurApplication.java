944da2cef47ca9714beb75052f3e62b3bb164d5f
Anas El Hani <elhanianas@gmail.com>
2020-11-27 08:03:56 +0100

    first commit

---
+++ src/main/java/com/example/demo/AdditionneurApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionneurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionneurApplication.class, args);
+	}
+
+}
